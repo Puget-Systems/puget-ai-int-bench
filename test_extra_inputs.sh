ab93b68f75cc55ff6e6a981b247a1950c571347c
Dustin Moore <dustmoo@gmail.com>
2026-05-07 13:52:59 -0700

    chore: remove accidentally committed scratch files

--- test_extra_inputs.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-docker run --rm --net host nvcr.io/nvidia/tritonserver:25.04-py3-sdk genai-perf profile \
-  -m qwen3.6:35b \
-  --endpoint-type chat \
-  -u http://localhost:11434 \
-  --num-prompts 1 \
-  --extra-inputs '{"stream_options": {"include_usage": true}}'
